4fc6b3ca7e8f6734be8e3b73ba8d56d112485430
DR Subordinate <dr_subordinate@example.com>
2023-11-20 16:57:20 +0900

    titleタグのテキストに時計用を追加

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>タグ作成ツール</title>
+    <title>タグ作成ツール 時計用</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./dist/output.css" rel="stylesheet">
