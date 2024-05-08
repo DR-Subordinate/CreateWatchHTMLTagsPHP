2d2c50377cb50c009c59059508023f62b64a2a75
DR Subordinate <dr_subordinate@example.com>
2024-05-08 18:18:50 +0900

    Yahoo!ショッピング スマホ用説明文の修正
    修正指示通りに画像を差し替え

--- tags/yahoo-shopping-sp-description.php
+++ tags/yahoo-shopping-sp-description.php
@@ -1,6 +1,6 @@
 <?php if(!empty($_POST['img-url'])): ?>
 <a href="https://shopping.yahoo.co.jp/stores/wrap/bouncer.html?dest_path=https://brandacross.xsrv.jp/syousaigazou/<?php echo $_POST['img-url']; ?>.html" target="_blank" style="margin-bottom: 20px; display: block;">
-<img src="https://auctions.c.yimg.jp/images.auctions.yahoo.co.jp/image/dr001/snavi0/ez4ai2mjspd322phnvvsooaa5m/2095788753.jpg" border="0" width="100%"></a>
+<img src="https://shopping.c.yimg.jp/lib/brand-across/pict_link_yh.jpg" border="0" width="100%"></a>
 <br>
 <?php endif; ?>
 <?php if($_POST['add-wrapping-banner'] == "true"): ?>
