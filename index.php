ebfb89b09fb704185eea669c409017d6d2fe8b74
DR Subordinate <karin.pspink@gmail.com>
2025-11-04 16:07:26 +0900

    スタイルが反映されないので代わりにCDNを利用
    突如スタイルが反映されない不具合に見舞われたので
    代わりにCDNを利用

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>タグ作成ツール 時計用</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./dist/output.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body>
     <div class="w-[500px] m-3 border border-black">
