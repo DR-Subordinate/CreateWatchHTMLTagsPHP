bd20c16a5290ebda3f6937c1f2000e63d35b11ee
DR Subordinate <karin.pspink@gmail.com>
2025-11-04 16:31:31 +0900

    スタイルが反映されないので代わりにCDNを利用
    突如スタイルが反映されない不具合に見舞われたので
    代わりにCDNを利用

--- tag.php
+++ tag.php
@@ -4,7 +4,7 @@
     <title>タグ作成ツール</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./dist/output.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
       .ms-gothic {
         font-family: "MS Gothic", "ＭＳ ゴシック", "Yu Gothic", "Meiryo", sans-serif;
