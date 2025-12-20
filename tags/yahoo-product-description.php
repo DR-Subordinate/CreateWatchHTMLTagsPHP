c1f182e4046925a0311cbad86ce8edbcb4a91cf0
DR Subordinate <karin.pspink@gmail.com>
2025-12-20 17:12:29 +0900

    時計用にもヤフー商品説明のタグを追加

---
+++ tags/yahoo-product-description.php
@@ -0,0 +1,5 @@
+<?php if($_POST['return'] == "true"): ?>
+<center><a href="https://store.shopping.yahoo.co.jp/brand-across/henpin.html"><img src="https://shopping.c.yimg.jp/lib/brand-across/hepin_ok.jpg" width="80%"></a><br></center>
+<?php else: ?>
+<center><a href="https://store.shopping.yahoo.co.jp/brand-across/henpin.html"><img src="https://shopping.c.yimg.jp/lib/brand-across/hepin_ng.jpg" width="80%"></a><br></center>
+<?php endif; ?>
